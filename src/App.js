b5f59150d64bf32e36e95ced1a1bd47e2fd5ae06
gabibble <87248000+gabibble@users.noreply.github.com>
2022-03-01 17:43:42 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
