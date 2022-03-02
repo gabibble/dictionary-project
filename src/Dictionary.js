2ab56a937d1468ddac04febacc45820550d057b2
gabibble <87248000+gabibble@users.noreply.github.com>
2022-03-01 19:27:18 -0800

    api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  // documentation: https://dictionaryapi.dev/
+
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
